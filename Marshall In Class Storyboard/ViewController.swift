9c8e604e4aa340a66fe220b5e83b29579f0700f6
Kai Marshall <k.marshall@setonhill.edu>
2020-01-29 18:57:30 -0500

    I don't know if i did this commit right

---
+++ Marshall In Class Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marshall In Class Storyboard
+//
+//  Created by Kai Marshall on 1/29/20.
+//  Copyright © 2020 Kai Marshall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
